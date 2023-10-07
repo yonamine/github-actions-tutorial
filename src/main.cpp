c1238e0c3d8f718a2ad3550e8b695df23cc60599
yonamine <fsy0jpn@gmail.com>
2023-10-07 09:34:55 +0900

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ * @brief
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    std::cout << "Hello World!" << std::endl;
+    return EXIT_SUCCESS;
+}
